a9d18a6a35eb3c1c2b0b6ff314bfa076a05493c5
Nikola Bodor <Nikola.Bodor@gmail.com>
2025-11-28 16:24:20 +1300

    added onboarding and stlying for page

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,18 +1,21 @@
 import { Outlet } from 'react-router'
 import Navbar from './Navbar'
+import { useLocation } from 'react-router-dom'
 
 export default function Layout() {
+  const location = useLocation()
+  const noNavbarPaths = ['/', '/onboarding']
+
   return (
     <>
-      <header className='flex flex-col items-center'>
+      <header className="flex flex-col items-center">
         <h1>GlIGHFE!</h1>
       </header>
       <main className="pb-20">
         {/* Bottom padding to account for fixed navbar height */}
         <Outlet />
       </main>
-      <Navbar />
-      {/* <footer></footer> */}
+        {!noNavbarPaths.includes(location.pathname) ? <Navbar /> : null}
     </>
   )
 }
--- client/components/Onboarding.tsx
+++ client/components/Onboarding.tsx
@@ -1,4 +1,53 @@
+import { useEffect } from 'react'
+
 function Onboarding() {
-  return <div>Onboarding Page</div>
+  const Glyph: string = 'Glyph-ee' // make setState to change through different pronounciations!!!
+
+  useEffect(() => {
+    // Lock scrolling when component mounts
+    document.body.style.overflow = 'hidden'
+    // Unlock scrolling when component unmounts
+    return () => {
+      document.body.style.overflow = 'unset'
+    }
+  }, [])
+
+  return (
+    <div className="font-body relative flex h-screen min-h-screen flex-col items-center overflow-hidden bg-gradient-to-b from-green-200 to-green-500 p-4 text-center">
+      <h1 className="mb-5 text-xl font-bold underline">What is GlIFGHE</h1>
+      <p className="rounded-xl border-2 border-black">
+        GlIFGHE (pronounced {`${Glyph}`}) is a social media platform that communicates
+        through pure visual signal: icons, symbols, and images designed for
+        universal interpretation—no language required.
+      </p>
+      <br />
+      <p className="rounded-xl border-2 border-black">
+        Within this system, text does not exist. No words, No captions, No
+        written markers of any kind.
+      </p>
+      <br />
+      <p className="rounded-xl border-2 border-black">
+        GlIFGHE leverages visual communication to drive authentic user
+        engagement. zero text, zero barriers. See with eyes. Think with heart.
+        Your journey begins.
+      </p>
+      <br />
+      <p className="rounded-xl border-2 border-black font-bold">
+        THAR BE NO WORDS NOR TEXT BEYOND THIS POINT, ON ME HONOR!
+      </p>
+      <br />
+      <button
+        onClick={() => {
+          window.location.href = '/feed'
+        }}
+        className="hover:bg-success-strong focus:ring-success-medium shadow-xs ml-7 box-border rounded-xl border-2 border-black bg-lime-400 p-0.5 px-4 py-2.5 text-sm font-medium leading-5 focus:outline-none focus:ring-4"
+      >
+        I AGREE I WILL USE NO TEXT
+      </button>
+      <footer className="right-0p-4 absolute bottom-0 left-0 text-white"></footer>
+    </div>
+  )
 }
 export default Onboarding
+
+// THERE SHALL BE NO WORDS OR TEXT BEYOND THIS POINT.
