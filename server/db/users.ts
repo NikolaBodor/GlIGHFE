5c5bdfb7124771e0fd5079d99d74e42a1e5c91e2
Matt <mattlocktom@gmail.com>
2025-11-28 15:36:16 +1300

    feat(db): Add DB functions for user profile, posts, and followers

--- server/db/users.ts
+++ server/db/users.ts
@@ -27,3 +27,45 @@ export async function addUser({
   })
   return result
 }
+
+// Get basic User info for profiles
+export async function getUserProfile(id: number): Promise<User> {
+  const user = await db('users')
+    .select('id', 'name', 'bio', 'profile_picture as profilePicture')
+    .where({ id })
+    .first()
+  return user
+}
+
+// Retrieve all posts from User
+export async function getUserPosts(id: number): Promise<Post[]> {
+  const posts = await db('posts')
+    .select(
+      'id',
+      'user_id as userId',
+      'message',
+      'image',
+      'date_added as dateAdded',
+    )
+    .where('user_id', id)
+    .orderBy('date_added', 'desc')
+  return posts
+}
+
+// Get followers of User
+export async function getFollowers(userId: number): Promise<User[]> {
+  const followers = await db('followers')
+    .join('users', 'followers.follower_id', 'users.id')
+    .where('followers.following_id', userId)
+    .select('users.id', 'users.name', 'users.profile_picture as profilePicture')
+  return followers
+}
+
+// Get who User is following
+export async function getFollowing(userId: number): Promise<User[]> {
+  const following = await db('followers')
+    .join('users', 'followers.following_id', 'users.id')
+    .where('followers.follower_id', userId)
+    .select('users.id', 'users.name', 'users.profile_picture as profilePicture')
+  return following
+}
