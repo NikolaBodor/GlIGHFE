134d7d0f330e64b9323f97199e81caa671a53473
Patrick Hignell <patrickhignell@hotmail.com>
2025-11-30 13:50:09 +1300

    fix error in main classname

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router'
 import Navbar from './Navbar'
 
 export default function Layout() {
-  const font = 'Wingdings'
+  const font = 'sans'
+  // const font = 'Wingdings'
   return (
     <>
       <header className={`flex flex-col items-center font-[${font}]`}>
         <h1>GlIGHFE!</h1>
       </header>
-      <main className={`}pb-20 font-[${font}]`}>
+      <main className={`pb-20 font-[${font}]`}>
         {/* Bottom padding to account for fixed navbar height */}
         <Outlet />
       </main>
