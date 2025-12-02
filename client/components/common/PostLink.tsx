3db9ffb99d2ca71f4e7622f1c76e43376ffe1087
Sofia Tarn <doseoftheplagues@gmail.com>
2025-12-02 15:50:57 +1300

    feat: add individual profile page and backend

---
+++ client/components/common/PostLink.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router'
+
+interface UserProfileLinkProps {
+  children: React.ReactNode
+  id: number
+  onClick?: () => void
+}
+
+export const PostLink: React.FC<UserProfileLinkProps> = ({
+  children,
+  onClick,
+  id,
+}) => {
+  // If there's no auth_id, just render the children without a link
+
+  return (
+    <Link to={`/post/${id}`} onClick={onClick}>
+      {children}
+    </Link>
+  )
+}
+
+export default PostLink
